7441aadc243bfc97817f161c74095c47762ee977
erica vella <evellanoweth@gmail.com>
2021-11-03 22:03:10 -0700

    added tweet class
    returns a list of tweets - work w them as you wish

--- wingspan/tanalysis/views.py
+++ wingspan/tanalysis/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Tweets
 
 # Create your views here.
+def index(request):
+    try:
+        twit_launcher = Tweets()
+        tweets = twit_launcher.search_tweets("ruggs", 20)
+    except:
+        tweets = []
+    html = "<html><body> %s </body></html>" % tweets
+    return HttpResponse(html)
