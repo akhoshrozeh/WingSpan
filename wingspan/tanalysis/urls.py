537f848589fb86881534923134a89d1518348173
Jason Rauchwerk <jrauchwerk@gmail.com>
2021-11-10 17:13:05 -0800

    Simple backend api - GET request with parameter query returns list of Tweet JSON objects

--- wingspan/tanalysis/urls.py
+++ wingspan/tanalysis/urls.py
@@ -17,5 +17,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('api', views.api, name='api')
 ]
