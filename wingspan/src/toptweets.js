2615ddacf9513eed6a5e6e093b8eb4e077e44234
Jason Rauchwerk <jrauchwerk@gmail.com>
2021-11-15 11:31:29 -0800

    Added TopTweets component with weird css

---
+++ wingspan/src/toptweets.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./toptweets.css";
+import TweetEmbed from 'react-tweet-embed';
+
+class TopTweets extends React.Component {
+    constructor(props) {
+        /* Calls React Component Constructor */
+        super(props);
+
+		/* Constructor */
+            this.tweets = props.ids.map((tid) => <TweetEmbed id={tid} />);
+    }
+
+    render() {
+        return (
+            <div class="toptweets">
+                    {this.tweets}
+            </div>
+        );
+    }
+}
+export default TopTweets;
