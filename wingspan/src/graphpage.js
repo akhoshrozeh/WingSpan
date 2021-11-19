48a25e564ac68190fa00929a92619034669956aa
Jason Rauchwerk <jrauchwerk@gmail.com>
2021-11-19 11:00:49 -0800

    graphpage sample data timestamps are now in the same format as twitter timestamps

--- wingspan/src/graphpage.js
+++ wingspan/src/graphpage.js
@@ -11,16 +11,16 @@ import './graphpage.css';
 import './background.css';
 
 const result = {top_tweets: [{id:"20", engagement:10}, {id:"1460657276142895123", engagement:20}],
-                scores: [{timestamp: '2021-11-11', score: 50}, 
-                         {timestamp: '2021-11-12', score: 11},
-                         {timestamp: '2021-11-13', score: 20},
-                         {timestamp: '2021-11-14', score: 40},
-                         {timestamp: '2021-11-15', score: 100},
-                         {timestamp: '2021-11-16', score: 7},
-                         {timestamp: '2021-11-17', score: 30},
-                         {timestamp: '2021-11-18', score: 70},
-                         {timestamp: '2021-11-19', score: 0},
-                         {timestamp: '2021-11-20', score: 90}]};
+                scores: [{timestamp: '2021-11-11T12:34:56Z', score: 50}, 
+                         {timestamp: '2021-11-12T12:34:56Z', score: 11},
+                         {timestamp: '2021-11-18T18:53:26Z', score: 20},
+                         {timestamp: '2021-11-14T18:53:26Z', score: 40},
+                         {timestamp: '2021-11-15T18:53:26Z', score: 100},
+                         {timestamp: '2021-11-16T18:53:26Z', score: 7},
+                         {timestamp: '2021-11-17T18:53:26Z', score: 30},
+                         {timestamp: '2021-11-19T19:53:26Z', score: 70},
+                         {timestamp: '2021-11-19T18:53:26Z', score: 0},
+                         {timestamp: '2021-11-19T18:55:26Z', score: 90}]};
 
 class GraphPage extends Component 
 {
