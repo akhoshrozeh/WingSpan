36a962a707e78c45c9bbadabb059d155afdf9f86
Richard Lee <richard@Richards-MacBook-Air.local>
2021-11-20 16:08:33 -0800

    rebase

--- wingspan/src/graphpage.js
+++ wingspan/src/graphpage.js
@@ -10,7 +10,9 @@ import TopTweets from './toptweets.js';
 import './graphpage.css';
 import './background.css';
 
-const result = {top_tweets: [{id:"20", engagement:10}, {id:"1460657276142895123", engagement:20}],
+const result = {
+                top_tweets: [{id:"20", engagement:10}, 
+                             {id:"1460657276142895123", engagement:20}],
                 scores: [{timestamp: '2021-11-11T12:34:56Z', score: 50}, 
                          {timestamp: '2021-11-12T12:34:56Z', score: 11},
                          {timestamp: '2021-11-18T18:53:26Z', score: 20},
@@ -20,10 +22,11 @@ const result = {top_tweets: [{id:"20", engagement:10}, {id:"1460657276142895123"
                          {timestamp: '2021-11-17T18:53:26Z', score: 30},
                          {timestamp: '2021-11-19T19:53:26Z', score: 70},
                          {timestamp: '2021-11-19T18:53:26Z', score: 0},
-                         {timestamp: '2021-11-19T18:55:26Z', score: 90}]};
+                         {timestamp: '2021-11-19T18:55:26Z', score: 90}]
+                };
 
 class GraphPage extends Component 
-{
+{   
 	/* Render the graph page, add page url with query once data is retrieved form Express server */
 	render() 
 	{
@@ -39,7 +42,7 @@ class GraphPage extends Component
 						<Header/> 
 					</div>
 					<div className = "graphcontainer">
-						<Graph data={result.scores}/>
+						<Graph data = {result.scores}/>
 					</div>
                     <div className = "tweetcontainer">
                         <TopTweets ids={result.top_tweets}/>
