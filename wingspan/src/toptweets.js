9a92c6270cff07af2a2bb89b51acd9bd03f94b51
Jason Rauchwerk <jrauchwerk@gmail.com>
2021-11-22 23:26:17 -0800

    Limits to only 20 top tweets

--- wingspan/src/toptweets.js
+++ wingspan/src/toptweets.js
@@ -5,6 +5,7 @@ class TopTweets extends Component {
     showTweets(ids) {
         let tweets = JSON.parse(JSON.stringify(ids));  // Deep copy
         tweets.sort((a,b) => b.engagement - a.engagement);
+        tweets = tweets.slice(0, 20);
         return tweets.map((tweet) => <TweetEmbed id={tweet.tid} key={tweet.tid} />);
     }
 
