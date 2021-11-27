37bed4defe00e47dcc3b5f3a8d590cd61d80b409
Jason Rauchwerk <jrauchwerk@gmail.com>
2021-11-26 21:09:43 -0800

    Searchbar tests

--- wingspan/src/toptweets.test.js
+++ wingspan/src/toptweets.test.js
@@ -4,14 +4,13 @@ import '@testing-library/jest-dom';
 import TopTweets from './toptweets';
 
 test('renders 0 tweets without crashing', () => {
-  const { container, getByText } = render(<TopTweets ids={[]}/>);
-  expect(container.childNodes.length).toBe(1);
-  expect(getByText('No Top Tweets')).toBeInTheDocument();
+  const { getByRole } = render(<TopTweets ids={[]}/>);
+  expect(getByRole('heading', {name: "No Top Tweets"})).toBeInTheDocument();
 });
 
 test('renders multiple tweets without crashing', () => {
   const ids = [{tid:"20", engagement:1}, {tid:"1464338231768539141", engagement:2}];
-  const { container, queryByText } = render(<TopTweets ids={ids}/>);
-  expect(container.childNodes.length).toBe(2);
-  expect(queryByText('No Top Tweets')).not.toBeInTheDocument();
+  const { container, queryByRole } = render(<TopTweets ids={ids}/>);
+  expect(container.childNodes.length).toBe(ids.length);
+  expect(queryByRole('heading', {name: "No Top Tweets"})).not.toBeInTheDocument();
 });
