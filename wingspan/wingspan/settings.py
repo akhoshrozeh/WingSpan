c9f34c45eadb6db1cf99f19963779ea8f981ed04
Anthony Khoshrozeh <akhoshrozeh@gmail.com>
2021-11-28 02:18:28 -0800

    coverage for tanalysis module

--- wingspan/wingspan/settings.py
+++ wingspan/wingspan/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_nose',
 ]
 
 MIDDLEWARE = [
@@ -55,6 +56,15 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'wingspan.urls'
+
+# Use nose to run all tests
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+# Tell nose to measure coverage on the 'foo' and 'bar' apps
+NOSE_ARGS = [
+    '--with-coverage',
+    '--cover-package=tanalysis',
+]
 
 TEMPLATES = [
     {
