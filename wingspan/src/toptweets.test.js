2e9a2069966cef79487d952eeb83760ef56f8a28
Jason Rauchwerk <jrauchwerk@gmail.com>
2021-11-27 21:30:56 -0800

    Fixed toptweets test based on new message in toptweets

--- wingspan/src/toptweets.test.js
+++ wingspan/src/toptweets.test.js
@@ -5,12 +5,12 @@ import TopTweets from './toptweets';
 
 test('renders 0 tweets without crashing', () => {
   const { getByRole } = render(<TopTweets ids={[]}/>);
-  expect(getByRole('heading', {name: "No Top Tweets"})).toBeInTheDocument();
+  expect(getByRole('heading', {name: /Top Tweets/})).toBeInTheDocument();
 });
 
 test('renders multiple tweets without crashing', () => {
   const ids = [{tid:"20", engagement:1}, {tid:"1464338231768539141", engagement:2}];
   const { container, queryByRole } = render(<TopTweets ids={ids}/>);
   expect(container.childNodes.length).toBe(ids.length);
-  expect(queryByRole('heading', {name: "No Top Tweets"})).not.toBeInTheDocument();
+  expect(queryByRole('heading', {name: /Top Tweets/})).not.toBeInTheDocument();
 });
