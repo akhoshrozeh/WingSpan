621c5afa752b6441a1f5d0d8ae4498c8374884c2
Anthony Khoshrozeh <akhoshrozeh@gmail.com>
2021-11-28 20:30:59 -0800

    setup.sh fixed

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-. ~/.nvm/nvm.sh
-. ~/.profile
-. ~/.bashrc
-. $(brew --prefix nvm)/nvm.sh  # if installed via Brew
-nvm install 17.1.0
-nvm use 17.1.0
-cd wingspan
+cd ../wingspan
 npm install 
 pip install -r ../requirements.txt
